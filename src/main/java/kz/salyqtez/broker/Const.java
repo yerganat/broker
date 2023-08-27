9baaff781c249403a63484606e74f5abb4dc7533
yerganat.merzentay <yerganat.merzentay@btsdigital.kz>
2023-08-27 20:32:33 +0600

    fixes

--- src/main/java/kz/salyqtez/broker/Const.java
+++ src/main/java/kz/salyqtez/broker/Const.java
@@ -3,6 +3,6 @@ package kz.salyqtez.broker;
 public class Const {
     public static String youtubeLinkName = "youtube";
     public static String youtubeLink = "https://www.youtube.com/watch?v=DAZjyll_PXM";
-    public static String myBotUserId = String.valueOf(423763358);
-    public static String spMailRu = "brokertaxcalculatorbot@mail.ru";
+    public static String myBotUserId = String.valueOf(546511553);
+    public static String spMailRu = "aidar.m@salyqtez.kz";
 }
